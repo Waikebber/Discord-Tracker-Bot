8566030e4e09c4212b98656c58ebb150f3b39160
Waikebber <waikebber@gmail.com>
2025-07-01 19:01:00 -0400

    added port binding

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
+import express from 'express';
 import { startBot } from './app/discord/bot.js';
 
-startBot();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => res.send('OK'));
+app.listen(port, () => {
+  console.log(`HTTP server listening on port ${port}`);
+});
+
+startBot()
+  .catch(err => {
+    console.error('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nBot failed to start:', err);
+    process.exit(1);
+  });
